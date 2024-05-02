a69bda9656cd3cb15173c45b2776f93c954f9baf
Victor Hugo <victorhugobbpereira02@gmail.com>
2024-05-02 08:10:27 -0300

    inicio do perojeto

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:acadia/src/app/my_home_page.dart';
+import 'package:flutter/material.dart';
+import 'package:window_manager/window_manager.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await windowManager.ensureInitialized();
+  WindowOptions windowOptions = const WindowOptions(
+    size: Size(800, 600),
+    center: true,
+    backgroundColor: Colors.red,
+    skipTaskbar: false,
+  );
+  windowManager.waitUntilReadyToShow(windowOptions, () async {
+    await windowManager.show();
+    await windowManager.focus();
+  });
+  runApp(const MyApp());
+}
